cf5c99d2d2ef9edc809a42269a838b44e4c95aeb
Jesus Mejia <jesmga24@gmail.com>
2024-11-15 14:53:51 -0600

    Created Model model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_15_170604) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_15_172909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,4 +20,15 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_15_170604) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "models", force: :cascade do |t|
+    t.string "name"
+    t.integer "average_price"
+    t.bigint "brand_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brand_id"], name: "index_models_on_brand_id"
+  end
+
+  add_foreign_key "models", "brands"
 end
---
+++ spec/factories/models.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :model do
+    name { "MyString" }
+    average_price { 1 }
+    brand { nil }
+  end
+end
