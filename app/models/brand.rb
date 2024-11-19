c74c74504bebf15186c4b69d8afb4a5802020dad
Jesus Mejia <jesmga24@gmail.com>
2024-11-19 14:45:56 -0600

    Added dynamic calculation of Brands' average_price on model creation or update

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,9 @@
 class Brand < ApplicationRecord
   has_many :models
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  def update_average_price
+    self.average_price = models.where("average_price IS NOT NULL").average(:average_price).to_i
+    save!
+  end
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,12 @@
 class Model < ApplicationRecord
   belongs_to :brand
+
+  # Updates the Brand's average price after a model is added or updated
+  after_save :update_brand_average_price
+
+  private
+
+  def update_brand_average_price
+    brand.update_average_price
+  end
 end
