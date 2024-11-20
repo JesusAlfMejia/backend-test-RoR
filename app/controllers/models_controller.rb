a45e2afa1706f2528b15d9d66e0c4be66577e395
Jesus Mejia <jesmga24@gmail.com>
2024-11-20 11:21:16 -0600

    Added functionality for GET /models

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,7 +1,31 @@
 class ModelsController < ApplicationController
+  # GET /models
+  # Returns a list of the models depending on the parameters given
+  # Returns an error message if the param greater is higher than param lower, or if the passed parameters are not integer
   def index
+    models = Model.all
+
+    # Validate params
+    greater = Integer(params[:greater]) rescue nil
+    lower = Integer(params[:lower]) rescue nil
+    if params[:greater].present? && greater.nil? || params[:lower].present? && lower.nil?
+      render json: { error: "Invalid parameters" }, status: :bad_request
+      return
+    end
+    if greater && lower && greater > lower
+      render json: { error: "greater cannot be higher than lower" }, status: :bad_request
+      return
+    end
+
+    # Filter results based on params
+    models = models.where("COALESCE(average_price, 0) > ?", greater) if greater
+    models = models.where("COALESCE(average_price, 0) < ?", lower) if lower
+    render json: ModelsRepresenter.new(models).as_json, status: :ok
   end
 
+  # PUT /brands
+  # Updates a model's average price
+  # Return error message if average_price is not higher than 100,000
   def update
     model = Model.find(params[:id])
     model.update!(model_params)
--- spec/requests/models_spec.rb
+++ spec/requests/models_spec.rb
@@ -33,4 +33,60 @@ RSpec.describe "Models", type: :request do
       expect(json_response["error"]).to include("Average price must be greater than 100000")
     end
   end
+
+  describe "GET /models" do
+    before do
+      brand1 = FactoryBot.create(:brand, name: "Acura")
+      brand2 = FactoryBot.create(:brand, name: "Toyota")
+      FactoryBot.create(:model, name: "ILX", average_price: "300000", brand_id: brand1.id)
+      FactoryBot.create(:model, name: "MDX", average_price: "400000", brand_id: brand1.id)
+      FactoryBot.create(:model, name: "RDX", average_price: "390000", brand_id: brand1.id)
+      FactoryBot.create(:model, name: "RLX", average_price: "450000", brand_id: brand1.id)
+      FactoryBot.create(:model, name: "TL", brand_id: brand1.id)
+      FactoryBot.create(:model, name: "Avanza", average_price: "160000", brand_id: brand2.id)
+      FactoryBot.create(:model, name: "Camry", average_price: "260000", brand_id: brand2.id)
+      FactoryBot.create(:model, name: "Corolla", average_price: "180000", brand_id: brand2.id)
+      FactoryBot.create(:model, name: "FJ Cruiser", brand_id: brand2.id)
+      FactoryBot.create(:model, name: "Hiace", brand_id: brand2.id)
+    end
+
+    it "shows all the models" do
+      get "/models"
+      expect(response).to have_http_status(:success)
+      json_response = JSON.parse(response.body)
+      expect(json_response.size).to eq(10)
+      expect(json_response[0]["name"]).to eq("Avanza")
+      expect(json_response[9]["name"]).to eq("TL")
+      expect(json_response[9]["average_price"]).to eq(0)
+    end
+
+    it "shows models within a range" do
+      get "/models?greater=380000&lower=400000"
+      expect(response).to have_http_status(:success)
+      json_response = JSON.parse(response.body)
+      expect(json_response.size).to eq(1)
+      expect(json_response[0]["name"]).to eq("RDX")
+      expect(json_response[0]["average_price"]).to eq(390000)
+    end
+
+    it "shows models higher than parameter given" do
+      get "/models?greater=250000"
+
+      expect(response).to have_http_status(:success)
+      json_response = JSON.parse(response.body)
+      expect(json_response.size).to eq(5)
+      expect(json_response[0]["name"]).to eq("Camry")
+      expect(json_response[4]["name"]).to eq("RLX")
+    end
+
+    it "shows models lower than parameter given" do
+      get "/models?lower=260000"
+
+      expect(response).to have_http_status(:success)
+      json_response = JSON.parse(response.body)
+      expect(json_response.size).to eq(5)
+      expect(json_response[0]["name"]).to eq("Avanza")
+      expect(json_response[4]["average_price"]).to eq(0)
+    end
+  end
 end
