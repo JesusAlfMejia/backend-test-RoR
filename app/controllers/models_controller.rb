942b3a19cf19e0d155f1f85cd2614e45c5148e09
Jesus Mejia <jesmga24@gmail.com>
2024-11-20 07:42:37 -0600

    Added functionality for PUT /models/:id

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,4 +1,16 @@
 class ModelsController < ApplicationController
   def index
   end
+
+  def update
+    model = Model.find(params[:id])
+    model.update!(model_params)
+    render json: { message: "Model updated successfully", id: model.id, name: model.name, average_price: model.average_price }, status: :ok
+  end
+
+  private
+
+  def model_params
+    params.require(:model).permit(:average_price)
+  end
 end
--- spec/requests/models_spec.rb
+++ spec/requests/models_spec.rb
@@ -1,11 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "Models", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/models/index"
-      expect(response).to have_http_status(:success)
+  describe "PUT /models/:id" do
+    let!(:brand) { FactoryBot.create(:brand, name: "Acura") }
+    let!(:model1) { FactoryBot.create(:model, name: "ILX", average_price: 200000, brand_id: brand.id) }
+    let!(:model2) { FactoryBot.create(:model, name: "MDX", average_price: 350000, brand_id: brand.id) }
+
+    it "updates a model's average price" do
+      brand.reload
+      expect {
+        put "/models/#{model1.id}", params: { average_price: 500000 }, as: :json
+        brand.reload
+        model1.reload
+      }.to change { model1.average_price }.from(200000).to(500000)
+      .and change { brand.average_price }.from(275000).to(425000)
+
+      expect(response).to have_http_status(:ok)
+      json_response = JSON.parse(response.body)
+      expect(json_response["message"]).to eq("Model updated successfully")
+      expect(json_response["id"]).to eq(model1.id)
+      expect(json_response["name"]).to eq("ILX")
+    end
+
+    it "throws an error if average_price is not higher than 100,000" do
+      expect {
+        put "/models/#{model1.id}", params: { average_price: 1000 }, as: :json
+        model1.reload
+      }.not_to change { model1.average_price }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      json_response = JSON.parse(response.body)
+      expect(json_response["error"]).to include("Average price must be greater than 100000")
     end
   end
-
 end
