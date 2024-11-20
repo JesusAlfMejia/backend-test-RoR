c0111c131c58d588d36ef2538d7a8d84c890d4e0
Jesus Mejia <jesmga24@gmail.com>
2024-11-20 12:44:58 -0600

    Added seeding to populate the database using models.json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,24 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+require 'json'
+
+# Read models.json file
+file_path = Rails.root.join('db', 'models.json')
+json_data = JSON.parse(File.read(file_path))
+
+# Create brands and models
+json_data.each do |model|
+  brand = Brand.find_by(name: model["brand_name"]) || Brand.create!(name: model["brand_name"])
+  model = Model.new(
+    id: model["id"],
+    name: model["name"],
+    average_price: model["average_price"],
+    brand_id: brand.id
+  )
+  model.save(validate: false)
+end
+
+# Reset PostgreSQL sequence
+ActiveRecord::Base.connection.reset_pk_sequence!('models')
